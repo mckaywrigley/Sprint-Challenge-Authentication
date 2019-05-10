a4ce619e359bb24c83a94d87b1d948bffac36e44
Mckay <mckaywrigley@gmail.com>
2019-05-10 09:04:17 -0700

    adds logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import { BrowserRouter, Link, Route } from "react-router-dom";
@@ -6,25 +6,30 @@ import Register from "./Register";
 import Login from "./Login";
 import Jokes from "./Jokes";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dad Jokes</h1>
-      <BrowserRouter>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/jokes">Jokes</Link>
-        <button>Logout</button>
-        <Route
-          exact
-          path="/register"
-          render={props => <Register {...props} />}
-        />
-        <Route exact path="/login" render={props => <Login {...props} />} />
-        <Route exact path="/jokes" render={props => <Jokes {...props} />} />
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  logout() {
+    localStorage.removeItem("jwt");
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Dad Jokes</h1>
+        <BrowserRouter>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/jokes">Jokes</Link>
+          <button onClick={this.logout}>Logout</button>
+          <Route
+            exact
+            path="/register"
+            render={props => <Register {...props} />}
+          />
+          <Route exact path="/login" render={props => <Login {...props} />} />
+          <Route exact path="/jokes" render={props => <Jokes {...props} />} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
