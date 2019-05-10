b2c49d35e11baed2ab8c8931860e509a23ec6461
Mckay <mckaywrigley@gmail.com>
2019-05-10 08:16:54 -0700

    login user

--- config/routes.js
+++ config/routes.js
@@ -27,6 +27,36 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  db("users")
+    .where({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: `${token}`
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err.response);
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
 
 function getJokes(req, res) {
